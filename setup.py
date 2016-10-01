74d186c28d65e13676eb9ae64cb2ca877ece35d9
Lk Geimfari <likid.geimfari@gmail.com>
2016-10-01 13:13:10 +0300

    Updated setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     name='church',
     version=__version__,
     packages=['church'],
-    keywords=['fake', 'data', 'testing', 'generate', 'faker'],
+    keywords=['fake', 'data', 'testing', 'generate', 'faker', 'church'],
     package_data={
         'church': [
             'data/*/*',
@@ -17,12 +17,15 @@ setup(
     author='Likid Geimfari',
     author_email='likid.geimfari@gmail.com',
     description='Church is a library that help you generate fake data.',
+    long_description="Church is a library to generate fake data."
+                     "It's very useful when you need to bootstrap your database.",
     classifiers=[
-        "Development Status :: 3 - Alpha",
-        'Programming Language :: Python :: 3',
+        "Development Status :: 4 - Beta",
+        'Intended Audience :: Developers',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'License :: OSI Approved :: MIT License',
         "Topic :: Software Development :: Testing",
     ],
 )
