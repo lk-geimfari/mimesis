cb4a76cbdd1c554b419e1409c958633b0836252e
Lk Geimfari <likid.geimfari@gmail.com>
2016-10-26 20:33:14 +0300

    Structure of json files

--- examples/sample.py
+++ examples/sample.py
@@ -1,80 +1,88 @@
 # Test
 
-structure = {
-    "personal": {
-        "names": {
-            "male": [],
-            "female": [],
-        },
-        "surnames": [],
-        "gender": [],
-        "views_on": [],
-        "language": [],
-        "nationality": [],
-        "occupation": [],
-        "worldview": [],
-        "political_views": [],
-        "academic_degree": [],
-        "sexuality": [],
-        "favorite_movie": [],
-        "university": [],
-    },
-    "datetime": {
-        "day": {
-            "name": [],
-            "abbr": []
-        },
-        "month": {
-            "name": [],
-            "abbr": []
-        },
-        "periodicity": [],
-
-    },
-    "address": {
-        "city": [],
-        "country": {
-            "iso_code": [],
-            "name": []
-        },
-        "street": {
-            "name": [],
-            "suffix": []
-        },
-        "state": {
-            "name": [],
-            "abbr": []
-        }
-    },
-    "food": {
-        "fruits": [],
-        "vegetables": [],
-        "dishes": [],
-        "spices": [],
-        "drinks": []
-    },
-    "text": {
-        "text": [],
-        "quotes": [],
-        "color": [],
-        "words": {
-            "normal": [],
-            "bad": []
-        }
-    },
-    "business": {
-        "company": {
-            "name": [],
-            "type": {
-                "title": [],
-                "abbr": []
+personal = {
+    "names": {
+        "male": [],
+        "female": [],
+        "title": [
+            {
+                "male": [],
+                "female": []
             }
+        ]
+    },
+    "surnames": [],
+    "gender": [],
+    "views_on": [],
+    "language": [],
+    "nationality": [],
+    "occupation": [],
+    "worldview": [],
+    "political_views": [],
+    "academic_degree": [],
+    "sexuality": [],
+    "favorite_movie": [],
+    "university": []
+}
 
+address = {
+    "city": [],
+    "country": {
+        "iso_code": [],
+        "name": []
+    },
+    "street": {
+        "name": [],
+        "suffix": []
+    },
+    "state": {
+        "name": [],
+        "abbr": []
+    }
+}
+
+business = {
+    "company": {
+        "name": [],
+        "type": {
+            "title": [],
+            "abbr": []
         }
+    }
+}
+
+datetime = {
+    "day": {
+        "name": [],
+        "abbr": []
     },
-    "science": {
-        "scientist": [],
-        "article": [],
-        "chemical_elements": []
+    "month": {
+        "name": [],
+        "abbr": []
     },
+    "periodicity": []
+}
+
+food = {
+    "fruits": [],
+    "vegetables": [],
+    "dishes": [],
+    "spices": [],
+    "drinks": []
+}
+
+science = {
+    "scientist": [],
+    "article": [],
+    "chemical_elements": []
+}
+
+text = {
+    "text": [],
+    "quotes": [],
+    "color": [],
+    "words": {
+        "normal": [],
+        "bad": []
+    }
 }
