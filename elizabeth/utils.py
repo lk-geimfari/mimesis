e02481cf57c693f82651ac1a81f3bacd5f7fe29d
Amin Alaee <mohammadamin.alaee@gmail.com>
2016-11-30 19:38:36 +0330

    add Farsi language address

--- elizabeth/utils.py
+++ elizabeth/utils.py
@@ -25,6 +25,10 @@ SUPPORTED_LOCALES = {
         "name": "Spanish",
         "name_local": "Español"
     },
+    "fa": {
+        "name": "Farsi",
+        "name_local": "Farsi"
+    },
     "fi": {
         "name": "Finnish",
         "name_local": "Suomi"
@@ -90,6 +94,8 @@ def pull(file, locale='en'):
     | en - English                 | (data/en)    |
     +------------------------------+--------------+
     | ru - Russian                 | (data/ru)    |
+    +------------------------------+--------------+
+    | fa - Farsi                 | (data/fa)    |
     +------------------------------+--------------+
     | fi - Finnish                 | (data/fi)    |
     +------------------------------+--------------+
