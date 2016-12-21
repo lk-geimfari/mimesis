ba2f256500f069a6850a00d91e707035bb1692ee
Alessandro Martini <alessandrofmartini@gmail.com>
2016-12-21 14:38:54 -0200

    Added the tests for Code.cnpj

--- tests/test_data/test_code.py
+++ tests/test_data/test_code.py
@@ -60,3 +60,18 @@ class CodeTestCase(DummyCase):
         self.assertEqual(len(cpf_without_mask), 11, cpf_without_mask)
         non_numeric_digits = re.sub('\d', '', cpf_without_mask)
         self.assertEqual('', non_numeric_digits, non_numeric_digits)
+
+    def test_cnpj(self):
+        # test if the cnpj has 14 digits with the mask
+        cnpj_with_mask = self.generic.code.cnpj()
+        self.assertEqual(len(cnpj_with_mask), 18, cnpj_with_mask)
+        # test the mask
+        non_numeric_digits = re.sub('\d', '', cnpj_with_mask)
+        self.assertEqual('../-', non_numeric_digits, non_numeric_digits)
+        self.assertEqual(len(re.sub('\D', '', cnpj_with_mask)),
+                         14, cnpj_with_mask)
+        # test for the cnpj without mask
+        cnpj_without_mask = self.generic.code.cnpj(False)
+        self.assertEqual(len(cnpj_without_mask), 14, cnpj_without_mask)
+        non_numeric_digits = re.sub('\d', '', cnpj_without_mask)
+        self.assertEqual('', non_numeric_digits, non_numeric_digits)
