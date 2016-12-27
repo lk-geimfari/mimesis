ca82468dba2f6db8c639dce4a8ac01d845424d68
Likid Geimfari <likid.geimfari@gmail.com>
2016-12-27 19:20:12 +0300

    Converted interdata.py to package

---
+++ elizabeth/core/interdata/__init__.py
@@ -0,0 +1,12 @@
+# Here you can see data that used in all locales.
+# interdata is abbreviation of "International Data".
+
+from .address import *
+from .development import *
+from .hardware import *
+from .internet import *
+from .network import *
+from .personal import *
+from .science import *
+from .text import *
+from .transport import *
