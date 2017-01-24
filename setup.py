37579694a7b516b7556c921407f21487786dc0ce
Likid Geimfari <likid.geimfari@gmail.com>
2017-01-24 16:16:10 +0300

    Upd

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from elizabeth import __version__, \
 
 here = os.path.abspath(os.path.dirname(__file__))
 
-with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+with open(os.path.join(here, 'pypi_readme.rst'), encoding='utf-8') as f:
     long_description = '\n' + f.read()
 
 setup(
