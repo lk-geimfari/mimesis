42e2d71c7efba2a417cb01bacf89f53e817b091d
Likid Geimfari <likid.geimfari@gmail.com>
2017-01-30 14:50:47 +0300

    Removed pass form exceptions

--- elizabeth/exceptions.py
+++ elizabeth/exceptions.py
@@ -1,20 +1,10 @@
 class UnsupportedLocale(KeyError):
-    """
-    An exception that raised when a locale doesn't
-    supported.
-    """
-    pass
+    """An exception that raised when a locale doesn't supported."""
 
 
 class JSONKeyError(KeyError):
-    """
-    An exception that raised when JSON key doesn't exist
-    """
-    pass
+    """An exception that raised when JSON key doesn't exist"""
 
 
 class NotImplementedYet(NotImplementedError):
-    """
-    An exception that raised when NotImplementedError
-    """
-    pass
+    """An exception that raised when NotImplementedError"""
