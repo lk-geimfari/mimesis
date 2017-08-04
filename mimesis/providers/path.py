8518488fb6eebe4ca6ad31c91cb9f0657302b7c9
ruslanv <ruslan.global@yandex.ru>
2017-08-04 15:38:30 +0300

    - added a new constant PLATFORM - added 2 new platforms for Path() - refactored Path() functions. No more hardcode parameters.

--- mimesis/providers/path.py
+++ mimesis/providers/path.py
@@ -3,13 +3,17 @@ import sys
 from mimesis.data import FOLDERS, PROGRAMMING_LANGS, PROJECT_NAMES
 from mimesis.providers import BaseProvider
 from mimesis.providers.personal import Personal
+from mimesis.constants.platforms import PLATFORMS
 
 
 class Path(BaseProvider):
     """Class that provides methods and property for generate paths."""
-
     def __init__(self, platform=sys.platform, *args, **kwargs):
-        # TODO: platform should be a parameter
+        """
+        :param platform: Required platform type ('linux2', 'darwin', 'win32',
+        'win64').
+        Supported platforms: mimesis/constant/platforms.py
+        """
         super().__init__(*args, **kwargs)
         self.__p = Personal('en')
         self.platform = platform
@@ -22,10 +26,10 @@ class Path(BaseProvider):
         :Example:
             /
         """
-        if self.platform == 'win32':
-            return 'C:\\'
-        else:
-            return '/'
+        for platform in PLATFORMS:
+            if self.platform == PLATFORMS[platform]['name']:
+                root = PLATFORMS[platform]['root']
+                return root
 
     @property
     def home(self):
@@ -35,10 +39,10 @@ class Path(BaseProvider):
         :Example:
             /home/
         """
-        if self.platform == 'win32':
-            return self.root + 'Users\\'
-        else:
-            return self.root + 'home/'
+        for platform in PLATFORMS:
+            if self.platform == PLATFORMS[platform]['name']:
+                home = PLATFORMS[platform]['home']
+                return home
 
     def user(self, gender='female'):
         """Generate a random user.
@@ -62,8 +66,11 @@ class Path(BaseProvider):
         """
         folder = self.random.choice(FOLDERS)
         user = self.user(user_gender)
-        path = '\\' if self.platform == 'win32' else '/'
-        return (user + '{}' + folder).format(path)
+        for platform in PLATFORMS:
+            if self.platform == PLATFORMS[platform]['name']:
+                path_separator = PLATFORMS[platform]['path_separator']
+                users_folder = (user + '{}' + folder).format(path_separator)
+                return users_folder
 
     def dev_dir(self, user_gender='female'):
         """Generate a random path to development directory.
@@ -76,8 +83,13 @@ class Path(BaseProvider):
         dev_folder = self.random.choice(['Development', 'Dev'])
         stack = self.random.choice(PROGRAMMING_LANGS)
         user = self.user(user_gender)
-        path = '\\' if self.platform == 'win32' else '/'
-        return (user + '{}' + dev_folder + '{}' + stack).format(path, path)
+        for platform in PLATFORMS:
+            if self.platform == PLATFORMS[platform]['name']:
+                path_separator = PLATFORMS[platform]['path_separator']
+                dev_dir = (
+                    user + '{}' + dev_folder + '{}' + stack
+                ).format(path_separator, path_separator)
+                return dev_dir
 
     def project_dir(self, user_gender='female'):
         """Generate a random path to project directory.
@@ -88,5 +100,10 @@ class Path(BaseProvider):
             /home/sherika/Development/Falcon/mercenary
         """
         project = self.random.choice(PROJECT_NAMES)
-        path = '\\' if self.platform == 'win32' else '/'
-        return (self.dev_dir(user_gender) + '{}' + project).format(path)
+        for platform in PLATFORMS:
+            if self.platform == PLATFORMS[platform]['name']:
+                path_separator = PLATFORMS[platform]['path_separator']
+                project_dir = (
+                    self.dev_dir(user_gender) + '{}' + project
+                ).format(path_separator)
+                return project_dir
