978ec9b67cd23c6ae339fd7b4e73ffd8871e7714
Likid Geimfari <likid.geimfari@gmail.com>
2017-08-21 18:38:53 +0300

    Minor updates in BaseProvider

--- mimesis/providers/base.py
+++ mimesis/providers/base.py
@@ -1,18 +1,23 @@
 from random import Random
 
 from mimesis.utils import locale_info
+from mimesis.settings import DEFAULT_LOCALE
 
 
 class BaseProvider(object):
     """This is a base class for all providers."""
 
-    def __init__(self, locale='en', seed=None):
+    def __init__(self, locale=None, seed=None):
         """Base constructor for all providers.
 
         :param locale: Current locale. Default is 'en'.
         :param seed: Seed to all the random functions. Default is 'None'.
         """
-        self.locale = locale
+        if not locale:
+            self.locale = DEFAULT_LOCALE
+        else:
+            self.locale = locale
+
         self.seed = seed
         self.random = Random()
 
