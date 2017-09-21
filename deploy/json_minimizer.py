d9d2ac1e009ef42cc457ea40bad3ac4cd336ab7f
Likid Geimfari <likid.geimfari@gmail.com>
2017-09-21 22:07:15 +0300

    Updated json minimizer

--- deploy/json_minimizer.py
+++ deploy/json_minimizer.py
@@ -9,6 +9,8 @@ from os.path import (
 )
 import json
 
+import re
+
 
 class Minimizer(object):
     """Minify content of all json file for all locales.
@@ -24,11 +26,66 @@ class Minimizer(object):
         return splitext(file_name)[1] == '.json'
 
     @staticmethod
+    def __minify(string, strip_space=True):
+        """Author of this functions: Gerald Storer.
+        The code released under the MIT license.
+        """
+        tokenizer = re.compile('"|(/\*)|(\*/)|(//)|\n|\r')
+        end_slashes_re = re.compile(r'(\\)*$')
+
+        in_string = False
+        in_multi = False
+        in_single = False
+
+        new_str = []
+        index = 0
+
+        for match in re.finditer(tokenizer, string):
+            if not (in_multi or in_single):
+                tmp = string[index:match.start()]
+                if not in_string and strip_space:
+                    tmp = re.sub('[ \t\n\r]+', '', tmp)
+                new_str.append(tmp)
+            elif not strip_space:
+                new_str.append(' ' * (match.start() - index))
+
+            index = match.end()
+            val = match.group()
+
+            if val == '"' and not (in_multi or in_single):
+                escaped = end_slashes_re.search(string, 0, match.start())
+                if not in_string or (escaped is None or len(escaped.group()) % 2 == 0):
+                    in_string = not in_string
+                index -= 1
+            elif not (in_string or in_multi or in_single):
+                if val == '/*':
+                    in_multi = True
+                elif val == '//':
+                    in_single = True
+            elif val == '*/' and in_multi and not (in_string or in_single):
+                in_multi = False
+                if not strip_space:
+                    new_str.append(' ' * len(val))
+            elif val in '\r\n' and not (in_multi or in_string) and in_single:
+                in_single = False
+            elif not ((in_multi or in_single) or (val in ' \r\n\t' and strip_space)):
+                new_str.append(val)
+
+            if not strip_space:
+                if val in '\r\n':
+                    new_str.append(val)
+                elif in_multi or in_single:
+                    new_str.append(' ' * len(val))
+
+        new_str.append(string[index:])
+        return ''.join(new_str)
+
+    @staticmethod
     def minify(file_path):
         """Prepare files to minify.
         """
         with open(file_path, 'r') as f:
-            json_text = json.loads(f.read())
+            json_text = Minimizer().__minify(json.loads(f.read()))
             minimized = json.dumps(json_text, separators=(',', ':'))
 
         if len(file_path) > 0:
