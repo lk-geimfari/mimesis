a2d222b68f0f90a81909cc9dc24bdbcf23794ae2
Likid Geimfari <likid.geimfari@gmail.com>
2017-09-21 19:19:27 +0300

    v0.0.8

--- mimesis/__version__.py
+++ mimesis/__version__.py
@@ -1,6 +1,6 @@
 """Meta-data of Mimesis."""
 
-__version__ = '0.0.7'
+__version__ = '0.0.8'
 __title__ = 'mimesis'
 __description__ = 'Mimesis: mock data for developers.'
 __url__ = 'https://github.com/lk-geimfari/mimesis'
