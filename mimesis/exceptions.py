db0cb1443d9bb60e2aed185cced6c58004c489db
bjd2385 <bjd2385@aperiodicity.com>
2017-10-30 05:47:36 -0400

    Some basic code/comment updates

--- mimesis/exceptions.py
+++ mimesis/exceptions.py
@@ -1,22 +1,22 @@
 class UnsupportedLocale(KeyError):
-    """An exception that raised when a locale doesn't supported."""
+    """ Raised when a locale isn't supported. """
 
 
 class JSONKeyError(KeyError):
-    """An exception that raised when JSON key doesn't exist"""
+    """ Raised when a JSON key doesn't exist. """
 
 
 class WrongArgument(KeyError):
-    """An exception that raised when argument is wrong"""
+    """ Raised when argument is wrong. """
 
 
 class UnexpectedGender(WrongArgument):
-    """An exceptions that raised when gender is wrong"""
+    """ Raised when gender is wrong. """
 
 
 class UnsupportedAlgorithm(AttributeError):
-    """An exception that raised when user want to use unsupported algorithm"""
+    """ Raised when the user wants to use an unsupported algorithm. """
 
 
 class UndefinedSchema(ValueError):
-    """An exception that raised when schema if empty"""
+    """ Raised when schema is empty. """
