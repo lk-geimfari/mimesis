8a8e67d91d71acde1feb9b8def1e4abca297733f
Likid Geimfari <likid.geimfari@gmail.com>
2017-11-13 16:00:01 +0300

    Added Field()

--- mimesis/schema.py
+++ mimesis/schema.py
@@ -1,71 +1,52 @@
-import json
-from typing import Optional, Iterator
+from typing import Any
 
-from mimesis.decorators import type_to
-from mimesis.exceptions import UndefinedSchema
-from mimesis.providers.base import BaseProvider
-from mimesis.providers.generic import Generic
-from mimesis.typing import JSON
+from mimesis import Generic
+
+DATA_PROVIDERS = [
+    'address',
+    'business',
+    'clothing_sizes',
+    'code',
+    'cryptographic',
+    'datetime',
+    'development',
+    'file',
+    'food',
+    'games',
+    'hardware',
+    'internet',
+    'numbers',
+    'path',
+    'personal',
+    'science',
+    'text',
+    'transport',
+    'unit_system',
+]
 
 
-class Schema(BaseProvider):
-    """Class which helps generate data by schema using any
-    providers which supported by mimesis.
+class Field(object):
+    """Field for generating data using Schema().
     """
 
-    schema = {}  # type: dict
+    def __init__(self, locale: str = 'en') -> None:
+        self.locale = locale
+        self.gen = Generic(self.locale)
+        self.SUPPORTED = DATA_PROVIDERS
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.generic = Generic(self.locale)
+    def __call__(self, name, **kwargs) -> Any:
+        for provider in self.SUPPORTED:
+            if hasattr(self.gen, provider):
+                provider = getattr(self.gen, provider)
 
-    def __generate(self, schema: JSON = dict) -> JSON:
-        data = dict()
-        for k, v in schema.items():
-            if isinstance(v, dict):
-                data[k] = self.__generate(v)
-            elif isinstance(v, list):
-                data[k] = [self.__generate(i) for i in v]
-            else:
-                provider, method = v.split('.')
-                data[k] = getattr(
-                    getattr(self.generic, provider), method)()
-        return data
-
-    def load(self, path: Optional[str] = None,
-             schema: Optional[JSON] = None) -> 'Schema':
-        """Load schema from python dict or json file.
-
-        :param path: Path to file.
-        :param schema: Dictionary (schema).
-        """
-
-        if schema:
-            self.schema = schema
-        if path:
-            try:
-                with open(path, 'r') as f:
-                    self.schema = json.load(f)
-            except FileNotFoundError:
-                # modify message
-                raise FileNotFoundError(
-                    'File {path} is not found'.format(path=path))
-            except ValueError:
-                raise ValueError('Invalid json file!')
-        return self
-
-    @type_to(list, check_len=True)
-    def create(self, iterations: int = 1) -> Iterator[dict]:
-        """Fill schema using data generators of mimesis.
-
-        :param int iterations: Count of iterations.
-        :return: Filled schema.
-        :raises UndefinedSchema: if self.schema is empty dict.
-        """
-
-        if self.schema:
-            return map(lambda _: self.__generate(self.schema),
-                       range(iterations))
+                if name and hasattr(provider, name):
+                    return getattr(provider, name)(**kwargs)
         else:
-            raise UndefinedSchema(
-                'The schema is empty or not loaded.')
+            raise ValueError('Undefined field')
+
+    def __str__(self):
+        return '{}:{}'.format(
+            self.locale,
+            self.__class__.__name__,
+
+        )
