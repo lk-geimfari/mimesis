ef4038e8d7e5ef728d8db927d97dff6f47ff19a0
Likid Geimfari <likid.geimfari@gmail.com>
2017-11-16 13:50:48 +0300

    Added module for enums

---
+++ mimesis/enums.py
@@ -0,0 +1,23 @@
+from enum import Enum
+from random import choice
+
+
+class PortRange(Enum):
+    DEFAULT = (1, 65535)
+    WELL_KNOWN = (1, 1023)
+    EPHEMERAL = (49152, 65535)
+    REGISTERED = (1024, 49151)
+
+
+class Gender(Enum):
+    FEMALE = 'female'
+    MALE = 'male'
+    F = FEMALE
+    M = MALE
+    RANDOM = choice([FEMALE, MALE])
+
+
+class TitleType(Enum):
+    TYPICAL = 'typical'
+    ACADEMIC = 'academic'
+    RANDOM = choice([TYPICAL, ACADEMIC])
