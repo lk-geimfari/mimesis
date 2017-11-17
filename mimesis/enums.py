5079e09268abf66f1cdd65b10631a4ab7ec014b4
Likid Geimfari <likid.geimfari@gmail.com>
2017-11-17 22:29:07 +0300

    Allowed RANDOM for enums

--- mimesis/enums.py
+++ mimesis/enums.py
@@ -1,5 +1,31 @@
-from enum import Enum
+from enum import Enum, EnumMeta
 from random import choice
+
+
+class clsproperty(object):
+    """Based on answer from StackOverFlow.
+    """
+
+    def __init__(self, mget):
+        self.mget = mget
+
+    def __get__(self, obj, klass=None):
+        if klass is None:
+            klass = type(obj)
+        return self.mget.__get__(obj, klass)()
+
+    def __set__(self, obj, value):
+        raise AttributeError("Can't set attribute")
+
+
+class AllowRandom(EnumMeta):
+    """This metaclass allows an attribute `RANDOM` which
+    equal to random field of enum for his subclasses.
+    """
+
+    @clsproperty
+    def RANDOM(self) -> Enum:
+        return choice(list(self))
 
 
 class PortRange(Enum):
@@ -9,13 +35,17 @@ class PortRange(Enum):
     REGISTERED = (1024, 49151)
 
 
-class Gender(Enum):
+class Gender(Enum, metaclass=AllowRandom):
     FEMALE = 'female'
     MALE = 'male'
-    RANDOM = choice([FEMALE, MALE])
 
 
-class TitleType(Enum):
+class TitleType(Enum, metaclass=AllowRandom):
     TYPICAL = 'typical'
     ACADEMIC = 'academic'
-    RANDOM = choice([TYPICAL, ACADEMIC])
+
+
+class CardType(Enum, metaclass=AllowRandom):
+    MASTER_CARD = 'MasterCard'
+    VISA = 'Visa'
+    AMERICAN_EXPRESS = 'American Express'
