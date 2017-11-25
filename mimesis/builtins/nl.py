14ca12072b959bd72a1f4f266c0a3adefb281cfa
Joris de Vrede <jorisdevrede@gmail.com>
2017-11-25 03:18:15 -0800

    builtin specprovider for locale nl (#296)
    * added a builtin specprovider for the nl locale that can generate a bsn.
    
    * - Fixed import in builtin module
    - silenced flake8
    
    * fixed PR comments:
    - renamed NetherlandsSpecProvider
    - changed _is_valid_bsn to non-public function
    
    * - renamed meta class name of NetherlandsSpecProvider and added extra test for it.
    - added annotations to _is_valid_bsn

---
+++ mimesis/builtins/nl.py
@@ -0,0 +1,35 @@
+from mimesis.builtins.base import BaseSpecProvider
+
+
+class NetherlandsSpecProvider(BaseSpecProvider):
+    """Provides data specific to nl locale"""
+
+    class Meta:
+        name = 'netherlands_provider'
+
+    def bsn(self) -> str:
+        """Generate a random, but valid burgerservicenummer (BSN).
+
+        :returns: random BSN
+
+        :Example:
+            255159705
+        """
+
+        # a bsn is valid when its 9 digits pass the '11 test'
+        def _is_valid_bsn(number: str) -> str:
+            total = 0
+            multiplier = 9
+
+            for char in number:
+                multiplier = -multiplier if multiplier == 1 else multiplier
+                total += int(char) * multiplier
+                multiplier -= 1
+
+            return total % 11 == 0
+
+        sample = str(self.random.randint(100000000, 999999999))
+        while not _is_valid_bsn(sample):
+            sample = str(self.random.randint(100000000, 999999999))
+
+        return sample
