c40e17a69cbc40dc2aead707a109b888692e53a9
Likid Geimfari <likid.geimfari@gmail.com>
2017-11-30 04:34:19 +0300

    Added examples to __doc__ of enums

--- mimesis/enums.py
+++ mimesis/enums.py
@@ -4,8 +4,8 @@ from typing import Any
 
 
 class AllowRandom(EnumMeta):
-    """This metaclass allow method get_random_item() which
-    equal to random field of enum for his subclasses.
+    """This metaclass allows method ``get_random_item()`` which
+    returns random field of enum.
     """
 
     def get_random_item(self) -> Any:
@@ -13,6 +13,13 @@ class AllowRandom(EnumMeta):
 
 
 class PortRange(Enum):
+    """Value for a method ``Internet.port()`` which
+    takes parameter``port_range``.
+
+    Example:
+        >>> internet.port(port_range=PortRange.REGISTERED)
+        '8080'
+    """
     ALL = (1, 65535)
     WELL_KNOWN = (1, 1023)
     EPHEMERAL = (49152, 65535)
@@ -20,22 +27,49 @@ class PortRange(Enum):
 
 
 class Gender(Enum, metaclass=AllowRandom):
+    """Value for a lot of methods which
+    takes argument ``gender``.
+
+    Example:
+        >>> personal.full_name(gender=Gender.FEMALE)
+        'Jessica Larson'
+    """
     FEMALE = 'female'
     MALE = 'male'
 
 
 class TitleType(Enum, metaclass=AllowRandom):
+    """Value for a method ``Personal.title()`` which
+    takes parameter``title_type``.
+
+    Example:
+        >>> personal.title(title_type=TitleType.TYPICAL)
+        'Mr.'
+    """
     TYPICAL = 'typical'
     ACADEMIC = 'academic'
 
 
 class CardType(Enum, metaclass=AllowRandom):
+    """Value for a method ``Payment.credit_card_number()`` which
+    takes parameter ``card_type``.
+
+    Example:
+        >>> payment.credit_card_number(card_type=CardType.VISA)
+        '4116 9018 7744 0125'
+    """
     MASTER_CARD = 'MasterCard'
     VISA = 'Visa'
     AMERICAN_EXPRESS = 'American Express'
 
 
 class Algorithm(Enum, metaclass=AllowRandom):
+    """Value for methods which take argument ``algorithm``.
+
+    Example:
+        >>> cryptographic.hash(algorithm=Algorithm.MD5)
+        '6023658e8cc9c97edfd51cf2fabf1faf'
+    """
     MD5 = 'md5'
     SHA1 = 'sha1'
     SHA224 = 'sha224'
@@ -45,6 +79,13 @@ class Algorithm(Enum, metaclass=AllowRandom):
 
 
 class TLDType(Enum, metaclass=AllowRandom):
+    """Value for a method ``Internet.top_level_domain()`` which
+    takes parameter ``tld_type``.
+
+    Example:
+        >>> internet.top_level_domain(tld_type=TLDType.GEOTLD)
+        '.moscow'
+    """
     CCTLD = 'cctld'
     GTLD = 'gtld'
     GEOTLD = 'geotld'
@@ -53,6 +94,13 @@ class TLDType(Enum, metaclass=AllowRandom):
 
 
 class Layer(Enum, metaclass=AllowRandom):
+    """Value for method ``Internet.network_protocol()`` which
+    takes parameter ``layer``.
+
+    Example:
+        >>> internet.network_protocol(layer=Layer.APPLICATION)
+        'BitTorrent'
+    """
     APPLICATION = 'application'
     DATA_LINK = 'data_link'
     NETWORK = 'network'
@@ -63,6 +111,12 @@ class Layer(Enum, metaclass=AllowRandom):
 
 
 class FileType(Enum, metaclass=AllowRandom):
+    """Value for methods which takes parameter ``file_type``.
+
+    Example:
+        >>> file.extension(file_type=FileType.SOURCE)
+        '.py'
+    """
     SOURCE = 'source'
     TEXT = 'text'
     DATA = 'data'
@@ -74,6 +128,13 @@ class FileType(Enum, metaclass=AllowRandom):
 
 
 class MimeType(Enum, metaclass=AllowRandom):
+    """Value for method ``File.mime_type()`` which
+    takes parameter ``type_``.
+
+    Example:
+        >>> file.mime_type(type_=MimeType.APPLICATION)
+        'application/alto-directory+json'
+    """
     APPLICATION = 'application'
     AUDIO = 'audio'
     IMAGE = 'image'
@@ -83,27 +144,58 @@ class MimeType(Enum, metaclass=AllowRandom):
 
 
 class PrefixSign(Enum, metaclass=AllowRandom):
+    """Value for method ``UnitSystem.prefix()`` which
+    takes parameter ``sign``.
+
+    Example:
+        >>> unitsystem.prefix(sign=PrefixSign.NEGATIVE)
+        'yocto'
+    """
     POSITIVE = 'positive'
     NEGATIVE = 'negative'
 
 
 class CountryCode(Enum, metaclass=AllowRandom):
+    """Value for method ``Address.country_iso_code()`` which
+    takes parameter ``fmt``.
+
+    Example:
+        >>> address.country_iso_code(fmt=CountryCode.ISO3)
+        'AND'
+    """
     ISO2 = 'iso2'
     ISO3 = 'iso3'
     NUMERIC = 'numeric'
 
 
 class ISBNFormat(Enum, metaclass=AllowRandom):
+    """Value for method ``Code.isbn()`` which
+    takes parameter ``fmt``.
+
+    Example:
+        >>> code.isbn(fmt=ISBNFormat.ISBN10)
+        '1-64396-236-2'
+    """
     ISBN13 = 'isbn-13'
     ISBN10 = 'isbn-10'
 
 
 class EANFormat(Enum, metaclass=AllowRandom):
+    """Value for method ``Code.ean()`` which
+    takes parameter ``fmt``.
+
+    Example:
+        >>> code.ean(fmt=EANFormat.EAN8)
+        '09959590'
+    """
     EAN8 = 'ean-8'
     EAN13 = 'ean-13'
 
 
 class SocialNetwork(Enum, metaclass=AllowRandom):
+    """Value for method ``Personal.social_media_profile()`` which
+    takes parameter ``site``.
+    """
     FACEBOOK = 'facebook'
     TWITTER = 'twitter'
     INSTAGRAM = 'instagram'
@@ -111,6 +203,9 @@ class SocialNetwork(Enum, metaclass=AllowRandom):
 
 
 class UnitName(Enum, metaclass=AllowRandom):
+    """Value for method ``UnitSystem.unit()`` which
+    takes parameter ``name``.
+    """
     MASS = ('gram', 'gr')
     INFORMATION = ('byte', 'b')
     THERMODYNAMIC_TEMPERATURE = ('kelvin', 'K')
