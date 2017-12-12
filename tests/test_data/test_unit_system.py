80f6d67430d8fce7e4165271b11c2365a562992c
dy <dy.here@protonmail.com>
2017-12-12 12:16:49 +0300

    Added seed tests and fixes for units

--- tests/test_data/test_unit_system.py
+++ tests/test_data/test_unit_system.py
@@ -9,11 +9,10 @@ from mimesis.exceptions import NonEnumerableError
 
 
 @pytest.fixture
-def us():
+def _us():
     return UnitSystem()
 
 
-# TODO: Fill seed test cases
 @pytest.fixture
 def _seeded_us():
     return UnitSystem(seed=42)
@@ -43,22 +42,21 @@ def _seeded_us():
         UnitName.RADIOACTIVITY,
     ],
 )
-def test_unit(us, name):
-    result = us.unit(name)
+def test_unit(_us, name):
+    result = _us.unit(name)
     assert result in name.value
 
-    symbol = us.unit(name, symbol=True)
+    symbol = _us.unit(name, symbol=True)
     assert symbol in name.value
 
 
 def test_seeded_unit(_seeded_us):
     result = _seeded_us.unit(name=UnitName.FORCE, symbol=True)
-    # assert result ==
+    assert result == 'N'
     result = _seeded_us.unit()
-    # assert result ==
+    assert result == 'becquerel'
     result = _seeded_us.unit()
-    # assert result ==
-    pass
+    assert result == 'mole'
 
 
 @pytest.mark.parametrize(
@@ -69,21 +67,20 @@ def test_seeded_unit(_seeded_us):
         (PrefixSign.NEGATIVE, False),
     ],
 )
-def test_prefix(us, sign, symbol):
-    prefix = us.prefix(sign=sign, symbol=symbol)
+def test_prefix(_us, sign, symbol):
+    prefix = _us.prefix(sign=sign, symbol=symbol)
     prefixes = SI_PREFIXES_SYM if symbol else SI_PREFIXES
 
     assert prefix in prefixes[sign.value]
 
     with pytest.raises(NonEnumerableError):
-        us.prefix(sign='nil')
+        _us.prefix(sign='nil')
 
 
 def test_seeded_prefix(_seeded_us):
-    prefix = _seeded_us.prefix(sign=PrefixSign.NEGATIVE, symbol=True)
-    # assert result ==
-    prefix = _seeded_us.prefix()
-    # assert result ==
-    prefix = _seeded_us.prefix()
-    # assert result ==
-    pass
+    result = _seeded_us.prefix(sign=PrefixSign.NEGATIVE, symbol=True)
+    assert result == 'c'
+    result = _seeded_us.prefix()
+    assert result == 'tera'
+    result = _seeded_us.prefix()
+    assert result == 'peta'
