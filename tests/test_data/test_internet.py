4352b8e81ad8d2b6746f811626462358177bb0bd
dy <dy.here@protonmail.com>
2017-12-16 20:09:54 +0300

    Added seed tests and fixes for internet

--- tests/test_data/test_internet.py
+++ tests/test_data/test_internet.py
@@ -16,7 +16,6 @@ def net():
     return Internet()
 
 
-# TODO: Fill seed test cases
 @pytest.fixture
 def _seeded_net():
     return Internet(seed=42)
@@ -29,10 +28,9 @@ def test_emoji(net):
 
 def test_seeded_emoji(_seeded_net):
     result = _seeded_net.emoji()
-    # assert result ==
+    assert result == ':hotsprings:'
     result = _seeded_net.emoji()
-    # assert result ==
-    pass
+    assert result == ':clap:'
 
 
 def test_hashtags(net):
@@ -45,12 +43,11 @@ def test_hashtags(net):
 
 def test_seeded_hashtags(_seeded_net):
     result = _seeded_net.hashtags(quantity=2)
-    # assert result ==
+    assert result == ['#familyfun', '#beautiful']
     result = _seeded_net.hashtags()
-    # assert result ==
+    assert result[0] == '#instagramanet'
     result = _seeded_net.hashtags()
-    # assert result ==
-    pass
+    assert result[0] == '#family1st'
 
 
 def test_home_page(net):
@@ -60,12 +57,11 @@ def test_home_page(net):
 
 def test_seeded_home_page(_seeded_net):
     result = _seeded_net.home_page(tld_type=TLDType.GEOTLD)
-    # assert result ==
+    assert result == 'http://www.imagining.bar'
     result = _seeded_net.home_page()
-    # assert result ==
+    assert result == 'http://www.afterfuture.catalonia'
     result = _seeded_net.home_page()
-    # assert result ==
-    pass
+    assert result == 'http://www.boons.company'
 
 
 def test_subreddit(net):
@@ -84,12 +80,11 @@ def test_subreddit(net):
 
 def test_seeded_subreddit(_seeded_net):
     result = _seeded_net.subreddit(nsfw=True, full_url=True)
-    # assert result ==
+    assert result == 'http://www.reddit.com/r/distension'
     result = _seeded_net.subreddit()
-    # assert result ==
+    assert result == '/r/DoesAnybodyElse'
     result = _seeded_net.subreddit()
-    # assert result ==
-    pass
+    assert result == '/r/4chan'
 
 
 def test_user_agent(net):
@@ -99,25 +94,14 @@ def test_user_agent(net):
 
 def test_seeded_user_agent(_seeded_net):
     result = _seeded_net.user_agent()
-    # assert result ==
+    assert result.startswith('Mozilla/5.0 (Windows')
     result = _seeded_net.user_agent()
-    # assert result ==
-    pass
+    assert result.startswith('Mozilla/5.0 (Linux')
 
 
 def test_image_placeholder(net):
     result = net.image_placeholder(width=400, height=300)
     assert result is not None
-
-
-def test_seeded_image_placeholder(_seeded_net):
-    result = _seeded_net.image_placeholder(width=128, height=128)
-    # assert result ==
-    result = _seeded_net.image_placeholder()
-    # assert result ==
-    result = _seeded_net.image_placeholder()
-    # assert result ==
-    pass
 
 
 def test_stock_image(net):
@@ -133,12 +117,11 @@ def test_stock_image(net):
 
 def test_seeded_stock_image(_seeded_net):
     result = _seeded_net.stock_image(category='space', width=800, height=600)
-    # assert result ==
+    assert result == 'https://source.unsplash.com/category/space/800x600'
     result = _seeded_net.stock_image()
-    # assert result ==
+    assert result.endswith('objects/1900x1080')
     result = _seeded_net.stock_image()
-    # assert result ==
-    pass
+    assert result.endswith('buildings/1900x1080')
 
 
 def test_image_by_keyword(net):
@@ -151,12 +134,11 @@ def test_image_by_keyword(net):
 
 def test_seeded_image_by_keyword(_seeded_net):
     result = _seeded_net.image_by_keyword(keyword='word')
-    # assert result ==
+    assert result == 'https://source.unsplash.com/weekly?word'
     result = _seeded_net.image_by_keyword()
-    # assert result ==
+    assert result == 'https://source.unsplash.com/weekly?girl'
     result = _seeded_net.image_by_keyword()
-    # assert result ==
-    pass
+    assert result == 'https://source.unsplash.com/weekly?cat'
 
 
 @pytest.mark.parametrize(
@@ -177,12 +159,11 @@ def test_network_protocol(net, layer):
 
 def test_seeded_network_protocol(_seeded_net):
     result = _seeded_net.network_protocol(layer=Layer.TRANSPORT)
-    # assert result ==
+    assert result == 'TCP'
     result = _seeded_net.network_protocol()
-    # assert result ==
+    assert result == 'AFP'
     result = _seeded_net.network_protocol()
-    # assert result ==
-    pass
+    assert result == 'PPTP'
 
 
 def test_network_protocol_exception(net):
@@ -202,12 +183,11 @@ def test_ip_v4(net):
 
 def test_seeded_ip_v4(_seeded_net):
     result = _seeded_net.ip_v4(with_port=True)
-    # assert result ==
+    assert result == '57.12.140.125:14629'
     result = _seeded_net.ip_v4()
-    # assert result ==
+    assert result == '71.52.44.216'
     result = _seeded_net.ip_v4()
-    # assert result ==
-    pass
+    assert result == '16.15.47.111'
 
 
 def test_ip_v6(net):
@@ -217,10 +197,9 @@ def test_ip_v6(net):
 
 def test_seeded_ip_v6(_seeded_net):
     result = _seeded_net.ip_v6()
-    # assert result ==
+    assert result == '2001:3900:cce:8cd0:7d62:7248:4771:347a'
     result = _seeded_net.ip_v6()
-    # assert result ==
-    pass
+    assert result == '2001:2c83:d806:1045:f41:2ff8:6ff1:771f'
 
 
 def test_mac_address(net):
@@ -230,10 +209,9 @@ def test_mac_address(net):
 
 def test_seeded_mac_address(_seeded_net):
     result = _seeded_net.mac_address()
-    # assert result ==
+    assert result == '00:16:3e:1c:0c:8c'
     result = _seeded_net.mac_address()
-    # assert result ==
-    pass
+    assert result == '00:16:3e:3e:72:47'
 
 
 def test_http_method(net):
@@ -243,10 +221,11 @@ def test_http_method(net):
 
 def test_seeded_http_method(_seeded_net):
     result = _seeded_net.http_method()
-    # assert result ==
+    assert result == 'GET'
     result = _seeded_net.http_method()
-    # assert result ==
-    pass
+    assert result == 'GET'
+    result = _seeded_net.http_method()
+    assert result == 'PUT'
 
 
 @pytest.mark.parametrize(
@@ -267,12 +246,11 @@ def test_content_type(net, mime_type):
 
 def test_seeded_content_type(_seeded_net):
     result = _seeded_net.content_type(mime_type=MimeType.MESSAGE)
-    # assert result ==
+    assert result == 'Content-Type: message/vnd.wfa.wsc'
     result = _seeded_net.content_type()
-    # assert result ==
+    assert result == 'Content-Type: application/conference-info+xml'
     result = _seeded_net.content_type()
-    # assert result ==
-    pass
+    assert result == 'Content-Type: video/raw'
 
 
 def test_content_type_wrong_arg(net):
@@ -290,12 +268,11 @@ def test_http_status_code(net):
 
 def test_seeded_http_status_code(_seeded_net):
     result = _seeded_net.http_status_code(code_only=False)
-    # assert result ==
+    assert result == '429 Too Many Requests'
     result = _seeded_net.http_status_code()
-    # assert result ==
+    assert result == '205'
     result = _seeded_net.http_status_code()
-    # assert result ==
-    pass
+    assert result == '101'
 
 
 @pytest.mark.parametrize(
@@ -315,12 +292,11 @@ def test_top_level_domain(net, domain_type):
 
 def test_seeded_top_level_domain(_seeded_net):
     result = _seeded_net.top_level_domain(tld_type=TLDType.CCTLD)
-    # assert result ==
+    assert result == '.cz'
     result = _seeded_net.top_level_domain()
-    # assert result ==
+    assert result == '.ly'
     result = _seeded_net.top_level_domain()
-    # assert result ==
-    pass
+    assert result == '.gop'
 
 
 def test_top_level_domain_unsupported(net):
@@ -345,12 +321,11 @@ def test_port(net, range_, excepted):
 
 def test_seeded_port(_seeded_net):
     result = _seeded_net.port(port_range=PortRange.REGISTERED)
-    # assert result ==
+    assert result == 42929
     result = _seeded_net.port()
-    # assert result ==
+    assert result == 7297
     result = _seeded_net.port()
-    # assert result ==
-    pass
+    assert result == 1640
 
 
 def test_torrent_portal_category(net):
@@ -360,7 +335,6 @@ def test_torrent_portal_category(net):
 
 def test_seeded_category_of_website(_seeded_net):
     result = _seeded_net.category_of_website()
-    # assert result ==
+    assert result == 'Porn/Movies DVDR'
     result = _seeded_net.category_of_website()
-    # assert result ==
-    pass
+    assert result == 'Applications/UNIX'
