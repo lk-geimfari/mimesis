8dd8df4e89d6f2109474a3248ec77aebb234a585
Líkið Geimfari <likid.geimfari@gmail.com>
2017-12-18 20:02:31 +0300

    Update test_numbers.py

--- tests/test_data/test_numbers.py
+++ tests/test_data/test_numbers.py
@@ -9,11 +9,6 @@ def numbers():
     return Numbers()
 
 
-@pytest.fixture
-def _seeded_numbers():
-    return Numbers(seed=42)
-
-
 def test_floats(numbers):
     result = numbers.floats()
     assert len(result) == 100
@@ -24,13 +19,22 @@ def test_floats(numbers):
     assert isinstance(result, list)
 
 
-def test_seeded_floats(_seeded_numbers):
-    result = _seeded_numbers.floats(n=1)
-    assert result[0] == 0.6394267984578837
-    result = _seeded_numbers.floats()
-    assert result[0] == 0.21863797480360336
-    result = _seeded_numbers.floats()
-    assert result[0] == 0.8758529403781941
+@pytest.mark.parametrize(
+    'start, end', [
+        (1, 10),
+        (10, 20),
+        (20, 30),
+    ],
+)
+def test_integers(numbers, start, end):
+    result = numbers.integers(start=start, end=end)
+
+    assert max(result) <= end
+    assert min(result) >= start
+    assert isinstance(result, list)
+
+    element = numbers.random.choice(result)
+    assert isinstance(element, int)
 
 
 def test_primes(numbers):
@@ -60,40 +64,13 @@ def test_digit(numbers):
     assert isinstance(result, str)
 
 
-def test_seeded_digit(_seeded_numbers):
-    result = _seeded_numbers.digit(to_bin=True)
-    assert result == '0b1'
-    result = _seeded_numbers.digit()
-    assert result == 0
-    result = _seeded_numbers.digit()
-    assert result == 4
-
-
 def test_between(numbers):
     result = numbers.between(90, 100)
     assert result >= 90
     assert result <= 100
 
 
-def test_seeded_between(_seeded_numbers):
-    result = _seeded_numbers.between(90, 100)
-    assert result == 100
-    result = _seeded_numbers.between()
-    assert result == 115
-    result = _seeded_numbers.between()
-    assert result == 26
-
-
 def test_rating(numbers):
     result = numbers.rating(maximum=5.0)
     assert isinstance(result, float)
     assert (result >= 0) and (result <= 5.0)
-
-
-def test_seeded_rating(_seeded_numbers):
-    result = _seeded_numbers.rating(maximum=10.0)
-    assert result == 6.4
-    result = _seeded_numbers.rating()
-    assert result == 0.1
-    result = _seeded_numbers.rating()
-    assert result == 1.4
