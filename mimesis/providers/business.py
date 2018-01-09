2a64faa4b26866357708b1f7edf47b1a6afbf409
Likid Geimfari <likid.geimfari@gmail.com>
2018-01-09 05:29:56 +0300

    Optimized the Business data provider

--- mimesis/providers/business.py
+++ mimesis/providers/business.py
@@ -22,8 +22,8 @@ class Business(BaseDataProvider):
         :Example:
             Gamma Systems.
         """
-        companies = self._data['company']['name']
-        return self.random.choice(companies)
+        return self.random.choice(
+            self._data['company']['name'])
 
     def company_type(self, abbr: bool = False) -> str:
         """Get a random type of business entity.
@@ -34,10 +34,9 @@ class Business(BaseDataProvider):
         :Example:
             Incorporated.
         """
-        key = 'abbr' if abbr else 'title'
-        company_type = self._data['company']['type']
         return self.random.choice(
-            company_type.get(key),
+            self._data['company']['type'].get(
+                'abbr' if abbr else 'title'),
         )
 
     def copyright(self) -> str:
@@ -53,34 +52,39 @@ class Business(BaseDataProvider):
             self.company_type(True),
         )
 
-    def currency_iso_code(self, crypto: bool = False) -> str:
+    def currency_iso_code(self) -> str:
         """Get a currency code. ISO 4217 format.
 
-        :param crypto: Return ISO code of cryptocurrency.
         :return: Currency code.
 
         :Example:
             RUR.
         """
-        if crypto:
-            return self.random.choice(
-                CRYPTOCURRENCY_ISO_CODES)
-
         return self.random.choice(CURRENCY_ISO_CODES)
 
-    def currency_symbol(self, crypto: bool = False):
+    def cryptocurrency_iso_code(self) -> str:
+        """Get symbol of random cryptocurrency.
+
+        :return: Symbol of cryptocurrency.
+        """
+        return self.random.choice(CRYPTOCURRENCY_ISO_CODES)
+
+    def currency_symbol(self):
         """Get a currency symbol for current locale.
 
-        :param crypto: Return symbol of cryptocurrency.
         :return: Currency symbol.
+        """
+        return CURRENCY_SYMBOLS[self.locale]
+
+    def cryptocurrency_symbol(self) -> str:
+        """Get a cryptocurrency symbol.
+
+        :return: Symbol of cryptocurrency.
 
         :Example:
             Ƀ
         """
-        if crypto:
-            return self.random.choice(CRYPTOCURRENCY_SYMBOLS)
-
-        return CURRENCY_SYMBOLS[self.locale]
+        return self.random.choice(CRYPTOCURRENCY_SYMBOLS)
 
     def price(self, minimum: float = 10.00,
               maximum: float = 1000.00) -> str:
@@ -93,15 +97,8 @@ class Business(BaseDataProvider):
         :Example:
             599.99 $.
         """
-        currencies = CURRENCY_SYMBOLS
         price = self.random.uniform(minimum, maximum)
-
-        fmt = '{0:.2f} {1}'
-
-        if self.locale in currencies:
-            return fmt.format(price, currencies[self.locale])
-
-        return fmt.format(price, currencies['default'])
+        return '{0:.2f} {1}'.format(price, self.currency_symbol())
 
     def price_in_btc(self, minimum: float = 0, maximum: float = 2) -> str:
         """Generate random price in BTC.
