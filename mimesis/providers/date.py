10f8ecfcfb12ce78a2c4d034776e63964d06ed5a
e-vandenberg <erik.vandenberg@ucc.on.ca>
2018-08-25 14:34:10 -0400

    Revert "Added feature from issue-485"
    This reverts commit fa8161fae8df6e60c5b6d709f5622dad1cb5e4eb.

--- mimesis/providers/date.py
+++ mimesis/providers/date.py
@@ -2,7 +2,6 @@
 
 from calendar import monthrange, timegm
 from datetime import date, datetime, time
-import pytz
 
 from mimesis.data import GMT_OFFSETS, ROMAN_NUMS, TIMEZONES
 from mimesis.providers.base import BaseDataProvider
@@ -165,7 +164,6 @@ class Datetime(BaseDataProvider):
         :Example:
             Europe/Paris
         """
-
         return self.random.choice(TIMEZONES)
 
     def gmt_offset(self) -> str:
@@ -178,11 +176,10 @@ class Datetime(BaseDataProvider):
         """
         return self.random.choice(GMT_OFFSETS)
 
-    def datetime(self, humanized: bool = False, timezone: str = '', **kwargs) -> DateTime:
+    def datetime(self, humanized: bool = False, **kwargs) -> DateTime:
         """Generate random datetime.
 
         :param humanized: Readable representation.
-        :param timezone: Custom timezone.
         :param kwargs: Keyword arguments (start, end).
         :return: Datetime.
         :rtype: datetime.datetime
@@ -190,9 +187,7 @@ class Datetime(BaseDataProvider):
         :Example:
             March, 24 2002
         """
-
         fmt = '%Y-%m-%d %H:%M:%S'
-
         dt_str = '{date} {time}'.format(
             date=self.date(fmt='%Y-%m-%d', **kwargs),
             time=self.time(),
@@ -202,10 +197,6 @@ class Datetime(BaseDataProvider):
 
         if humanized:
             return dt.strftime('%B, %d %Y')
-
-        if timezone != '':
-            timezone = pytz.timezone(timezone)
-            dt = timezone.localize(dt)
 
         return dt
 
