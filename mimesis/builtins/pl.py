94665dc66d408afd30cdf5d5b82334d2840f8293
devaerial <devaerial@gmail.com>
2018-08-28 08:44:20 +0200

    fix: Few changes in PolishSpecProvider
    * Changed variable name for `args`
    * Changed `map` to list comprehensions
    * Year generation using `providers.Datetime`

--- mimesis/builtins/pl.py
+++ mimesis/builtins/pl.py
@@ -5,6 +5,7 @@ from typing import Optional
 
 from mimesis.builtins.base import BaseSpecProvider
 from mimesis.enums import Gender
+from mimesis.providers import Datetime
 from mimesis.typing import DateTime
 
 __all__ = ['PolandSpecProvider']
@@ -23,19 +24,21 @@ class PolandSpecProvider(BaseSpecProvider):
 
         :return: valid 10-digit NIP
         """
-        nip_digits = list(map(int, str(self.random.randint(101, 998))))
+        nip_digits = [int(d) for d in str(self.random.randint(101, 998))]
         for _ in range(6):
             nip_digits.append(self.random.randint(0, 9))
-        args = (6, 5, 7, 2, 3, 4, 5, 6, 7)
-        sum_v = sum(map(lambda x: x[0] * x[1], zip(args, nip_digits)))
+        nip_coefficients = (6, 5, 7, 2, 3, 4, 5, 6, 7)
+        sum_v = sum(
+            map(lambda x: x[0] * x[1], zip(nip_coefficients, nip_digits)))
         checksum_digit = sum_v % 11
-        while checksum_digit > 9:
+        if checksum_digit > 9:
             checksum_digit = 0
         nip_digits.append(checksum_digit)
         return ''.join(str(d) for d in nip_digits)
 
-    def pesel(self, birth_date: Optional[DateTime]=None,
-              gender: Optional[Gender]=None) -> str:
+    def pesel(self,
+              birth_date: Optional[DateTime] = None,
+              gender: Optional[Gender] = None) -> str:
         """Generate random 11-digit PESEL.
 
         :param birth_date: initial birth date (optional)
@@ -43,7 +46,7 @@ class PolandSpecProvider(BaseSpecProvider):
         :return: valid 11-digit PESEL
         """
         if not birth_date:
-            year = self.random.randint(1800, 2299) % 100
+            year = Datetime().year(1800, 2299) % 100
             month = self.random.randint(1, 12)
             if month in (4, 6, 9, 10):
                 day = self.random.randint(1, 30)
@@ -68,19 +71,20 @@ class PolandSpecProvider(BaseSpecProvider):
             month += 40
         elif 2200 <= year <= 2299:
             month += 60
-        pesel_digits += list(map(int, '{:02d}'.format(month)))
-        pesel_digits += list(map(int, '{:02d}'.format(day)))
+        pesel_digits += [int(d) for d in '{:02d}'.format(month)]
+        pesel_digits += [int(d) for d in '{:02d}'.format(day)]
         series_number = self.random.randint(0, 999)
-        pesel_digits += list(map(int, '{:03d}'.format(series_number)))
+        pesel_digits += [int(d) for d in '{:03d}'.format(series_number)]
         if gender == Gender.MALE:
             gender_digit = self.random.choice((1, 3, 5, 7, 9))
         elif gender == Gender.FEMALE:
             gender_digit = self.random.choice((0, 2, 4, 6, 8))
         else:
             gender_digit = self.random.choice(range(10))
-        pesel_digits += list(map(int, str(gender_digit)))
-        args = (9, 7, 3, 1, 9, 7, 3, 1, 9, 7)
-        sum_v = sum(map(lambda x: x[0] * x[1], zip(args, pesel_digits)))
+        pesel_digits += [int(d) for d in str(gender_digit)]
+        pesel_coefficients = (9, 7, 3, 1, 9, 7, 3, 1, 9, 7)
+        sum_v = sum(
+            map(lambda x: x[0] * x[1], zip(pesel_coefficients, pesel_digits)))
         checksum_digit = sum_v % 10
         pesel_digits.append(checksum_digit)
         return ''.join(str(d) for d in pesel_digits)
@@ -90,9 +94,10 @@ class PolandSpecProvider(BaseSpecProvider):
 
         :return: valid 9-digit REGON
         """
-        args = (8, 9, 2, 3, 4, 5, 6, 7)
+        regon_coefficients = (8, 9, 2, 3, 4, 5, 6, 7)
         regon_digits = [self.random.randint(0, 9) for x in range(8)]
-        sum_v = sum(map(lambda x: x[0] * x[1], zip(args, regon_digits)))
+        sum_v = sum(
+            map(lambda x: x[0] * x[1], zip(regon_coefficients, regon_digits)))
         checksum_digit = sum_v % 11
         if checksum_digit > 9:
             checksum_digit = 0
