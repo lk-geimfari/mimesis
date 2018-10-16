184ea26c267b8bb9ce1e82190b8a6dcd5d43b8aa
Líkið Geimfari <likid.geimfari@gmail.com>
2018-10-16 18:08:42 +0300

    Removed deprecated Structure.json() method

--- mimesis/providers/structure.py
+++ mimesis/providers/structure.py
@@ -1,7 +1,5 @@
 """Provides structured data."""
 
-import json
-
 from mimesis.data import (CSS_PROPERTIES, CSS_SELECTORS, CSS_SIZE_UNITS,
                           HTML_CONTAINER_TAGS, HTML_MARKUP_TAGS)
 from mimesis.providers.base import BaseDataProvider
@@ -126,45 +124,3 @@ class Structure(BaseDataProvider):
             raise NotImplementedError(
                 'Attribute type {} is not implemented'.format(value))
         return value
-
-    def json(self, items: int = 5,
-             max_depth: int = 3, recursive: bool = False) -> str:
-        """Generate a random snippet of JSON.
-
-        :param items: Number of top-level items to produce.
-        :param max_depth: Maximum depth of each top-level item.
-        :param recursive:
-            When used recursively, will return a Python object instead of JSON
-            string.
-        :return: JSON
-        """
-        root = self.random.choice([list, dict])()
-
-        for _ in range(items):
-
-            key = self.__text.word()
-
-            if max_depth > 0:
-                json_values = {
-                    'str': lambda: self.__text.sentence(),
-                    'int': lambda: self.random.randint(1, 10000),
-                    'float': lambda: self.random.random(),
-                    'bool': lambda: self.random.choice([True, False]),
-                    'null': lambda: None,
-                    'json': lambda: self.json(
-                        max_depth=max_depth - 1,
-                        recursive=True,
-                    ),
-                }
-                vkey = self.random.choice(list(json_values.keys()))
-                value = json_values[vkey]()
-
-                if isinstance(root, list):
-                    root.append(value)
-                elif isinstance(root, dict):
-                    root[key] = value
-
-        if recursive:
-            return root
-
-        return json.dumps(root, indent=4)
