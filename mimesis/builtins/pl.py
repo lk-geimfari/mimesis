54aee342c0cd2fa386df020d08b10c7a9af39ae9
Líkið Geimfari <likid.geimfari@gmail.com>
2018-11-04 11:34:36 +0300

    Minor style improvements in PolandSpecProvider

--- mimesis/builtins/pl.py
+++ mimesis/builtins/pl.py
@@ -24,10 +24,11 @@ class PolandSpecProvider(BaseSpecProvider):
         :return: Valid 10-digit NIP
         """
         nip_digits = [int(d) for d in str(self.random.randint(101, 998))]
-        nip_digits += [self.random.randint(0, 9) for x in range(6)]
+        nip_digits += [self.random.randint(0, 9) for _ in range(6)]
         nip_coefficients = (6, 5, 7, 2, 3, 4, 5, 6, 7)
-        sum_v = sum(
-            map(lambda x: x[0] * x[1], zip(nip_coefficients, nip_digits)))
+        sum_v = sum([nc * nd for nc, nd in
+                     zip(nip_coefficients, nip_digits)])
+
         checksum_digit = sum_v % 11
         if checksum_digit > 9:
             return self.nip()
@@ -45,11 +46,13 @@ class PolandSpecProvider(BaseSpecProvider):
         """
         date_object = birth_date
         if not date_object:
-            date_object = Datetime().datetime(1800, 2299)
+            date_object = Datetime().datetime(1940, 2018)
+
         year = date_object.date().year
         month = date_object.date().month
         day = date_object.date().day
         pesel_digits = [int(d) for d in str(year)][-2:]
+
         if 1800 <= year <= 1899:
             month += 80
         elif 2000 <= year <= 2099:
@@ -58,20 +61,23 @@ class PolandSpecProvider(BaseSpecProvider):
             month += 40
         elif 2200 <= year <= 2299:
             month += 60
+
         pesel_digits += [int(d) for d in '{:02d}'.format(month)]
         pesel_digits += [int(d) for d in '{:02d}'.format(day)]
         series_number = self.random.randint(0, 999)
         pesel_digits += [int(d) for d in '{:03d}'.format(series_number)]
+
         if gender == Gender.MALE:
             gender_digit = self.random.choice((1, 3, 5, 7, 9))
         elif gender == Gender.FEMALE:
             gender_digit = self.random.choice((0, 2, 4, 6, 8))
         else:
             gender_digit = self.random.choice(range(10))
+
         pesel_digits.append(gender_digit)
         pesel_coeffs = (9, 7, 3, 1, 9, 7, 3, 1, 9, 7)
-        sum_v = sum(
-            map(lambda x: x[0] * x[1], zip(pesel_coeffs, pesel_digits)))
+        sum_v = sum([nc * nd for nc, nd in
+                     zip(pesel_coeffs, pesel_digits)])
         checksum_digit = sum_v % 10
         pesel_digits.append(checksum_digit)
         return ''.join(str(d) for d in pesel_digits)
@@ -82,9 +88,9 @@ class PolandSpecProvider(BaseSpecProvider):
         :return: Valid 9-digit REGON
         """
         regon_coeffs = (8, 9, 2, 3, 4, 5, 6, 7)
-        regon_digits = [self.random.randint(0, 9) for x in range(8)]
-        sum_v = sum(
-            map(lambda x: x[0] * x[1], zip(regon_coeffs, regon_digits)))
+        regon_digits = [self.random.randint(0, 9) for _ in range(8)]
+        sum_v = sum([nc * nd for nc, nd in
+                     zip(regon_coeffs, regon_digits)])
         checksum_digit = sum_v % 11
         if checksum_digit > 9:
             checksum_digit = 0
