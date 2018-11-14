d06ceda9018d01530b1f6c6ad4e6c95eb02f255b
Líkið Geimfari <likid.geimfari@gmail.com>
2018-11-14 11:12:22 +0300

    Moved pull to base data provider

--- mimesis/providers/base.py
+++ mimesis/providers/base.py
@@ -1,11 +1,16 @@
 """Base data provider."""
 
-from typing import Any, Optional
+import collections
+import functools
+import json
+
+from os import path
+from typing import Any, Optional, Mapping
 
 from mimesis import locales
 from mimesis.exceptions import NonEnumerableError, UnsupportedLocale
 from mimesis.helpers import Random, get_random_item, random
-from mimesis.typing import Seed
+from mimesis.typing import JSON, Seed
 
 __all__ = ['BaseDataProvider', 'BaseProvider']
 
@@ -74,9 +79,11 @@ class BaseDataProvider(BaseProvider):
         :param seed: Seed to all the random functions.
         """
         super().__init__(seed=seed)
-        self._data: dict
-        self._datafile: str
+        self._data = {}
+        self._datafile: Optional[str] = None
         self._setup_locale(locale)
+        self._data_dir = path.abspath(
+            path.join('mimesis', 'data'))
 
     def _setup_locale(self, locale: Optional[str] = None) -> None:
         """Set up locale after pre-check.
@@ -93,6 +100,69 @@ class BaseDataProvider(BaseProvider):
             raise UnsupportedLocale(locale)
 
         self.locale = locale
+
+    def _update_dict(self, initial: JSON, other: Mapping) -> JSON:
+        """Recursively update a dictionary.
+
+        :param initial: Dict to update.
+        :param other: Dict to update from.
+        :return: Updated dict.
+        """
+        for key, value in other.items():
+            if isinstance(value, collections.Mapping):
+                r = self._update_dict(initial.get(key, {}), value)
+                initial[key] = r
+            else:
+                initial[key] = other[key]
+        return initial
+
+    @functools.lru_cache(maxsize=None)
+    def pull(self, data_dir: Optional[str] = None,
+             datafile: Optional[str] = None, locale: Optional[str] = None):
+        """Pull the content from the JSON and memorize one.
+
+        Opens JSON file ``file`` in the folder ``data/locale``
+        and get content from the file and memorize ones using lru_cache.
+
+        :param data_dir: Data directory.
+        :param datafile: The name of file.
+        :param locale: Locale.
+        :return: The content of the file.
+        :raises UnsupportedLocale: if locale is not supported.
+        """
+
+        if not data_dir:
+            data_dir = self._data_dir
+
+        if not datafile:
+            datafile = self._datafile
+
+        if not locale:
+            locale = self.locale
+
+        def get_data(locale_name: str) -> JSON:
+            """Pull JSON data from file.
+
+            :param locale_name: Locale name.
+            :return: Content of JSON file as dict.
+            """
+            file_path = path.join(data_dir, locale_name, datafile)
+            with open(file_path, 'r', encoding='utf8') as f:
+                return json.load(f)
+
+        locale = locale.lower()
+
+        if locale not in locales.SUPPORTED_LOCALES:
+            raise UnsupportedLocale(locale)
+
+        master_locale = locale.split('-')[0]
+        data = get_data(master_locale)
+
+        # Handle sub-locales
+        if '-' in locale:
+            data = self._update_dict(data, get_data(locale))
+
+        self._data = data
 
     def get_current_locale(self) -> str:
         """Get current locale.
