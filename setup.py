8dc98c5f293523dae8c70ad5449b7ca72dd1b27d
Likid Geimfari <likid.geimfari@gmail.com>
2019-12-14 02:46:44 +0300

    Add Python 3.8 to classifiers in setup.py

--- setup.py
+++ setup.py
@@ -152,6 +152,7 @@ setup(
         'Intended Audience :: Developers',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: Implementation :: CPython',
         'Operating System :: OS Independent',
         'Topic :: Software Development',
