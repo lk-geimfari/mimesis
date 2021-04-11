4dac526df98a63b2b5c28fd2194fd5985916c19e
Isaak Uchakaev <likid.geimfari@gmail.com>
2021-04-12 02:18:11 +0300

    Fix format.sh

--- scripts/format.sh
+++ scripts/format.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-poetry run isort mimesis tests --force-single-line-imports
+poetry run black mimesis tests --skip-string-normalization
+poetry run isort mimesis tests
 poetry run autoflake --remove-all-unused-imports --recursive --remove-unused-variables --in-place mimesis tests --exclude=__init__.py
-poetry run black mimesis tests
