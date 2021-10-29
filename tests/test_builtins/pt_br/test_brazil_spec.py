7cb1199d0904e3799a3695bdef465bc8021a2ff4
Isaak Uchakaev <likid.geimfari@gmail.com>
2021-10-30 00:56:55 +0300

    Fix pt_br

--- tests/test_builtins/pt_br/test_brazil_spec.py
+++ tests/test_builtins/pt_br/test_brazil_spec.py
@@ -15,14 +15,14 @@ def test_cpf(pt_br):
     assert len(cpf_with_mask) == 14
 
     # test the mask
-    non_numeric_digits = re.sub("\d", "", cpf_with_mask)
+    non_numeric_digits = re.sub(r"\d", "", cpf_with_mask)
     assert "..-" == non_numeric_digits == non_numeric_digits
-    assert len(re.sub("\D", "", cpf_with_mask)) == 11
+    assert len(re.sub(r"\D", "", cpf_with_mask)) == 11
 
     # test for the cpf without mask
     cpf_without_mask = pt_br.cpf(False)
     assert len(cpf_without_mask) == 11
-    non_numeric_digits = re.sub("\d", "", cpf_without_mask)
+    non_numeric_digits = re.sub(r"\d", "", cpf_without_mask)
     assert "" == non_numeric_digits
 
 
@@ -32,12 +32,12 @@ def test_cnpj(pt_br):
     assert len(cnpj_with_mask) == 18
 
     # test the mask
-    non_numeric_digits = re.sub("\d", "", cnpj_with_mask)
+    non_numeric_digits = re.sub(r"\d", "", cnpj_with_mask)
     assert "../-" == non_numeric_digits == non_numeric_digits
-    assert len(re.sub("\D", "", cnpj_with_mask)) == 14
+    assert len(re.sub(r"\D", "", cnpj_with_mask)) == 14
 
     # test for the cnpj without mask
     cnpj_without_mask = pt_br.cnpj(False)
     assert len(cnpj_without_mask) == 14
-    non_numeric_digits = re.sub("\d", "", cnpj_without_mask)
+    non_numeric_digits = re.sub(r"\d", "", cnpj_without_mask)
     assert "" == non_numeric_digits
