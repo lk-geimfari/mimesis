lk-geimfari/mimesis

commit 94eee4ebfd3c0bde76427e3b59fb65a5ac144595

Fix #1115 (#1116)

mimesis/providers/generic.py
@@ -112,6 +112,20 @@ class Generic(BaseProvider):
                     attributes.append(a)
         return attributes
 
+    def reseed(self, seed: Optional[Seed] = None) -> None:
+        """Reseed the internal random generator.
+
+        Overrides method `BaseProvider.reseed()`.
+
+        :param seed: Seed for random.
+        """
+        for attr in self.__dir__():
+            try:
+                provider = getattr(self, attr)
+                provider.reseed(seed)
+            except AttributeError:
+                continue
+
     def add_provider(self, cls: Type[BaseProvider], **kwargs: Any) -> None:
         """Add a custom provider to Generic() object.
 

tests/test_providers/test_generic.py
@@ -5,6 +5,13 @@ from mimesis import BaseProvider, Generic
 
 
 class TestGeneric(object):
+    def test_reseed(self, generic):
+        generic.reseed(0xFFF)
+        address_1 = generic.address.address()
+        generic.reseed(0xFFF)
+        address_2 = generic.address.address()
+        assert address_1 == address_2
+
     def test_base_person(self, generic):
         result = generic.person.username()
         assert result is not None

mimesis/__init__.py
@@ -72,7 +72,7 @@ __all__ = [
     "__license__",
 ]
 
-__version__ = "5.0.0"
+__version__ = "5.1.0"
 __title__ = "mimesis"
 __description__ = "Mimesis: fake data generator."
 __url__ = "https://github.com/lk-geimfari/mimesis"

pyproject.toml
@@ -1,6 +1,6 @@
 [tool.poetry]
 name = "mimesis"
-version = "5.0.0"
+version = "5.1.0"
 description = "Mimesis: fake data generator."
 authors = ["Isaak Uchakaev <likid.geimfari@gmail.com>"]
 license = "MIT"

commit ba413e45b035eff707f42c39936942bbd3e9beb1

Fix reseeding of random generator on `Generic`.
Regression in 94eee4ebfd3c0bde76427e3b59fb65a5ac144595.

This ensures that the `random` instance on `Generic` itself is reseeded
to allow reproducible use of standard random methods without using a
subprovider. This also ensures that instances of `Generic` have isolated
copies of the random number generator.

mimesis/providers/generic.py
@@ -117,6 +117,9 @@ class Generic(BaseProvider):
 
         :param seed: Seed for random.
         """
+        # Ensure that we reseed the random generator on Generic itself.
+        super().reseed(seed)
+
         for attr in self.__dir__():
             try:
                 provider = getattr(self, attr)

tests/test_providers/test_generic.py
@@ -7,9 +7,12 @@ from mimesis import BaseProvider, Generic
 class TestGeneric(object):
     def test_reseed(self, generic):
         generic.reseed(0xFFF)
+        number_1 = generic.random.uniform(0, 1000)
         address_1 = generic.address.address()
         generic.reseed(0xFFF)
+        number_2 = generic.random.uniform(0, 1000)
         address_2 = generic.address.address()
+        assert number_1 == number_2
         assert address_1 == address_2
 
     def test_base_person(self, generic):