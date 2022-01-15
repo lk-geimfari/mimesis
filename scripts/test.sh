60477e538fbe9f83ce5c2b710eb33dfc93718e38
Isaak Uchakaev <likid.geimfari@gmail.com>
2022-01-15 23:41:43 +0300

    Add pytest-randomly to scripts/test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,5 +2,5 @@
 
 poetry run mypy mimesis
 poetry run black mimesis tests --check --skip-string-normalization
-poetry run pytest --cov=mimesis --cov-report=xml
+poetry run pytest --cov=mimesis --cov-report=xml --randomly-seed=$RANDOM
 poetry run pyroma -d .
