a38a04e2c79c99734f2a93fee8f2351b63f93828
Isaak Uchakaev <likid.geimfari@gmail.com>
2022-01-23 13:51:39 +0300

    Add custom styles for docs

--- docs/conf.py
+++ docs/conf.py
@@ -173,6 +173,10 @@ html_favicon = './_static/favicon.png'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+html_css_files = [
+    'css/custom.css',
+]
+
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
 # directly to the root of the documentation.
