736827f395f85b9376daf9f7cececad9def1ffea
Isaak Uchakaev <likid.geimfari@gmail.com>
2022-06-22 15:26:22 +0300

    Remove unused dev dependency

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,4 +3,3 @@
 poetry run mypy mimesis
 poetry run black mimesis tests --check --skip-string-normalization
 poetry run pytest --cov=mimesis --cov-report=xml --randomly-seed=$RANDOM
-poetry run pyroma -d .
