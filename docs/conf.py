abd553aa5f9227be8a58fd8719b3c78ccf1531d8
Isaak Uchakaev <likid.geimfari@gmail.com>
2022-07-10 17:50:39 +0300

    Fix formatting in conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -79,7 +79,7 @@ release = "5.5.0"
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = 'en'
+language = "en"
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
