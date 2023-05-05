8d26a1f752c1d2089c58084379a534768e1c24e5
Isaak Uchakaev <likid.geimfari@gmail.com>
2023-05-05 12:16:20 +0300

    Fix serve command

--- scripts/serve-docs.sh
+++ scripts/serve-docs.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/env bash
 
+PORT=8888
+DOCS_DIR="docs/_build/html"
+
 open_url() {
-  open http://localhost:8888
+  open http://localhost:${PORT}
 }
 
 serve_docs() {
   open_url & # Open the URL in the background
-  python3 -m http.server 8888 -d docs/_build/html
+  python3 -m http.server ${PORT} -d ${DOCS_DIR}
 }
 
-if [ -d "docs/_build/html" ]; then
+if [ -d "${DOCS_DIR}" ]; then
   serve_docs
 else
   make docs && serve_docs
