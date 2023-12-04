1e0f51268d2826580fe6509ac766809514bc25e4
Isaak Uchakaev <likid.geimfari@gmail.com>
2023-12-04 13:15:16 +0300

    Update clean script

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -4,5 +4,5 @@ find . -name '*.pyc' -exec rm -f {} +
 find . -name '*.pyo' -exec rm -f {} +
 find . -name '*~' -exec rm -f {} +
 find . -name '__pycache__' -exec rm -fr {} +
-rm -rf build/ dist/ .cache/ .mypy_cache/ .pytest_cache/ .benchmarks/ docs/_build mimesis.egg-info/ .ipynb_checkpoints/
+rm -rf build/ dist/ .cache/ .mypy_cache/ .pytest_cache/ .benchmarks/ docs/_build mimesis.egg-info/ .ipynb_checkpoints/ .ruff_cache/
 rm -f .coverage coverage.xml
