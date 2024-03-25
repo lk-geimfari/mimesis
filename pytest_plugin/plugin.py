2217a64cc070fc51725a4dc96a6569a1e1f62942
Matthew Heguy <matthew.heguy@flo.com>
2024-03-25 12:45:31 -0400

    Add missing parts of PR

--- pytest_plugin/plugin.py
+++ pytest_plugin/plugin.py
@@ -1,21 +1,25 @@
-from typing import Callable
+from typing import TYPE_CHECKING, Callable, TypeAlias  # pragma: no cover
 
-from mimesis.locales import Locale
-from mimesis.schema import Field
+if TYPE_CHECKING:
+    from mimesis.locales import Locale
+    from mimesis.schema import Field
 
-try:
+try:  # pragma: no cover
     import pytest
-except ImportError:
+except ImportError:  # pragma: no cover
     raise ImportError("pytest is required to use this plugin")
 
-_CacheCallable = Callable[[Locale], Field]
+
+_CacheCallable: TypeAlias = "Callable[[Locale], Field]"  # pragma: no cover
 
 
 @pytest.fixture(scope="session")
 def _mimesis_cache() -> _CacheCallable:
-    cached_instances: dict[Locale, Field] = {}
+    from mimesis.schema import Field
 
-    def factory(locale: Locale) -> Field:
+    cached_instances: "dict[Locale, Field]" = {}
+
+    def factory(locale: "Locale") -> "Field":
         if locale not in cached_instances:
             cached_instances[locale] = Field(locale)
         return cached_instances[locale]
@@ -24,12 +28,14 @@ def _mimesis_cache() -> _CacheCallable:
 
 
 @pytest.fixture()
-def mimesis_locale() -> Locale:
+def mimesis_locale() -> "Locale":
     """Specifies which locale to use."""
+    from mimesis.locales import Locale
+
     return Locale.DEFAULT
 
 
 @pytest.fixture()
-def mimesis(_mimesis_cache: _CacheCallable, mimesis_locale: Locale) -> Field:
+def mimesis(_mimesis_cache: _CacheCallable, mimesis_locale: "Locale") -> "Field":
     """Mimesis fixture to provide fake data using all built-in providers."""
     return _mimesis_cache(mimesis_locale)
