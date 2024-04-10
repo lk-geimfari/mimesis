9df2209f6776f0af20ee8bc71b62f5329776cbf5
snyata <p4rlx-news@pm.me>
2024-04-10 18:47:46 +0930

    Cognitive Synthesis Created

---
+++ README.md
@@ -0,0 +1,7 @@
+# Cognitive Synthesis
+A generator of synthetic data of the nature that might be typical of an Adversarial AI engine for specialized model training dependent on the use case.
+
+
+# Requirements
+- Signs and Signatures of specific patterns related to Adversarial AI traffic.
+  - These will also contribute to the Orion Hunts 
---
+++ main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0.1"
+    }
+  }
+}
+
+provider "docker" {}
+
+resource "docker_image" "nginx" {
+  name         = "nginx"
+  keep_locally = false
+}
+
+resource "docker_container" "nginx" {
+  image = docker_image.nginx.image_id
+  name  = "tutorial"
+
+  ports {
+    internal = 80
+    external = 8000
+  }
+}
